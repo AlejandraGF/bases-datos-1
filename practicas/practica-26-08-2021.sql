bc2bd3447960db333dc25640251d3d289d36812a
AlejandraGF <74033751+AlejandraGF@users.noreply.github.com>
2021-08-31 16:54:20 -0600

    Update practica-26-08-2021.sql

--- practicas/practica-26-08-2021.sql
+++ practicas/practica-26-08-2021.sql
@@ -3,8 +3,8 @@ use star_wars;
 
 
 create table people (
-	id int auto_increment,
-	`name` varchar(50),
+    id int auto_increment,
+    `name` varchar(50),
     mass int,
     hair_color varchar(20),
     eye_color varchar(20),
@@ -19,9 +19,10 @@ select id,name from people;
 show create table people;
 describe people;
 
+select * from planet;
 create table if not exists planet(
-	id int primary key auto_increment,
-	`name` varchar(50),
+    id int primary key auto_increment,
+    `name` varchar(50),
     orbital_period int,
     rotation_period int,
     diameter int,
@@ -32,12 +33,12 @@ create table if not exists planet(
     population long
 );
 
-insert into people(name, mass, hair_color, eye_color, gender, planet_id) values ('Luke Skywalker', 77, 'blond', 'blue', 'male',1);
+insert into people(name, mass, hair_color, eye_color, gender,planet_id) values ('Luke Skywalker', 77, 'blond', 'blue', 'male',1);
 insert into people(name, mass, hair_color, eye_color, gender,planet_id) values ('Leia Skywalker', 70, 'brown', 'brown', 'female',1);
-insert into people(name, mass, hair_color, eye_color, gender) values ('Han Solo', 80, 'brown', 'brown', 'male');
+insert into people(name, mass, hair_color, eye_color, gender,planet_id) values ('Han Solo', 80, 'brown', 'brown', 'male',2);
 
-insert into planet (name, orbital_period, rotation_period, diameter, climate, gravity, terrain, surface_water,  population) 
-values ('Tatooine', 304, 23, 10465, 'arid', '1 standard', 'desert', 1, 200000);
+insert into planet (id,name, orbital_period, rotation_period, diameter, climate, gravity, terrain, surface_water,  population) 
+values (1,'Tatooine', 304, 23, 10465, 'arid', '1 standard', 'desert', 1, 200000);
 
 insert into planet (name, orbital_period, rotation_period, diameter, climate, gravity, terrain, surface_water,  population) 
 values ('Corellia', 304, 23, 10465, 'arid', '1 standard', 'desert', 1, 200000);
